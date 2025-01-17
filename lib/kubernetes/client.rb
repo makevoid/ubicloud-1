20eaf19e74c975fce4b72a8f8f0ad6483e6aac75
Eren Başak <eren@ubicloud.com>
2025-01-17 16:57:20 +0300

    Add kubernetes cluster and nodepool upgrades
    This patch implements the upgrade logic for kubernetes clusters.
    
    Upgrade is done sequentially, node-by-node, in the order of creation time.
    Upgrade operation starts with the CP nodes.
    
    Nodepools need to "incr_upgrade"d separately to start their upgrade process.
    
    For upgrading a node, it creates a new node with upgraded version of k8s, adds it to
    the cluster and drains/removes the corresponding old node. There's no actual relation
    between the old node and new node in reality.
    
    Co-authored-by: Eren Başak <eren@ubicloud.com>
    Co-authored-by: mohi-kalantari <mohi.kalantari1@gmail.com>

--- lib/kubernetes/client.rb
+++ lib/kubernetes/client.rb
@@ -34,6 +34,14 @@ class Kubernetes::Client
     @session.exec!("sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf #{cmd}")
   end
 
+  def version
+    @session.exec!("kubectl version --client")
+  end
+
+  def delete_node(node_name)
+    kubectl("delete node #{node_name}")
+  end
+
   def set_load_balancer_hostname(svc, hostname)
     patch_data = JSON.generate({
       "status" => {
